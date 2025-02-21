bc73c9e84b0d4af67363f119c8cb1a4d46bb6ad5
Bryant <bryanttejeda628f@hotmail.com>
2025-02-21 16:54:00 -0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   const [countries, setCountries] = useState([]);
   const [countrySelected, setCountrySelected] = useState("");
   const [dataCountry, setDataCountry] = useState({})
-  function chargeInfoCountry(country) {
+  function chargeInfoCountry(country) {    
     setCountrySelected(country);    
   }
 
@@ -20,11 +20,9 @@ export default function App() {
       })
   }, []);
 
-  useEffect(() => {
-
-    let CountryFilter = countries.filter((country) => country.name.common === countrySelected );
+  useEffect(() => {    
+    let CountryFilter = countries.filter((country) => country.name.common === countrySelected);
     setDataCountry(CountryFilter);
-    console.log(CountryFilter);
     
   },[countrySelected, countries])
 
@@ -38,9 +36,14 @@ export default function App() {
        
       </header>
     
-    {countrySelected === "" ? 
-    <AllCountries chargeInfoCountry={chargeInfoCountry} countries={countries}/> :
-    <Country dataCountry={dataCountry} />}
+      {countrySelected === "" ? (
+      <AllCountries chargeInfoCountry={chargeInfoCountry} countries={countries} />
+    ) : (
+      dataCountry && Object.keys(dataCountry).length > 0 ? (
+        <Country dataCountry={dataCountry} />
+      ) : null
+    )}
+
     </div>
   )
 }
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -3,12 +3,33 @@ import PropTypes from "prop-types"
 import { useEffect, useState } from "react"
 export default function Country({ dataCountry }){
 
+    useEffect(() => {
+        console.log(dataCountry)
+    },[dataCountry]);
+
     return (
         <div className="countryContainer">
             <div className="imgCountry"
-                 style={{ backgroundImage: `url(${dataCountry.flags.png})` }}></div>
+                style={{ backgroundImage: dataCountry[0].flags.svg ? `url(${dataCountry[0].flags.svg})` : ""}}></div>
 
-            <h1>h1</h1>
+            <h1>{dataCountry[0].name.common}</h1>
+            <span>{dataCountry[0].name.official}</span>
+            <div className="containerInfoItem">
+                <div className="infoItem">
+                    <span>Population</span>
+                    <div className="lineH"></div>
+                    <span>{dataCountry[0].population}</span>
+                </div>
+                <div className="infoItem">
+                    <span>Area (km²)</span>
+                    <div className="lineH"></div>
+                    <span>{dataCountry[0].area}</span>
+                </div>
+
+                <div className="otherInfo">
+                    
+                </div>
+            </div>
         </div>
     )
 }
